0cc209ce935254e99b00f81225c4729e0ba1e3ca
ethanhkim <ethan.kim@mail.utoronto.ca>
2020-02-28 14:57:33 -0500

    Adding from 2/27

--- R Scripts/Preprocessing He et al.R
+++ R Scripts/Preprocessing He et al.R
@@ -1,10 +1,9 @@
-#Preprocessing He et al
+#Preprocessing He et al data to change Ensembl ID to Gene ID
 
 #Load required libraries
 
 library(tidyverse)
 library(org.Hs.eg.db)
-library(biomaRt)
 library(org.Mmu.eg.db)
 
 DS2_Human <- read.table("./Data/He et al/DS2_sectionRPKM_human.tsv")
@@ -13,29 +12,26 @@ DS1_Human <- read.table("./Data/He et al/tab.average_RPKM_section_human.tsv")
 DS1_Macaque <- read.table("./Data/He et al/tab.average_RPKM_section_macaque.tsv")
 DS1_Chimp <- read.table("./Data/He et al/tab.average_RPKM_section_chimp.tsv")
 
-#Extract Ensembl ID from row
-DS1_Human_ensembl_id <- rownames(DS1_Human)
-DS1_Human$ENSEMBL_ID <- DS1_Human_ensembl_id
-
-DS1_Macaque_ensembl_id <- rownames(DS1_Macaque)
-DS1_Macaque$ENSEMBL_ID <- DS1_Macaque_ensembl_id
+#Provided Ensembl ID are all the same; refer to error here:
 
 #Truncate the version numbers on ENSEMBL ID
-DS2_Human_RPKM_cleaned <- gsub("\\.\\d+$", "", DS2_Human$V1)
-DS2_Macaque_cleaned <- gsub("\\.\\d+$", "", DS2_Macaque$V1)
-DS1_Human$ENSEMBL_ID <- gsub("\\.\\d+$", "", DS1_Human$ENSEMBL_ID)
-DS1_Macaque$ENSEMBL_ID <- gsub("\\.\\d+$", "", DS1_Macaque$ENSEMBL_ID)
+DS2_Human$V1 <- gsub("\\.\\d+$", "", DS2_Human$V1)
+DS2_Macaque$V1 <- gsub("\\.\\d+$", "", DS2_Macaque$V1)
+DS1_Human$V1 <- gsub("\\.\\d+$", "", DS1_Human$V1)
+DS1_Macaque$V1 <- gsub("\\.\\d+$", "", DS1_Macaque$V1)
+DS1_Chimp$V1 <- gsub("\\.\\d+$", "", DS1_Chimp$V1)
 
-#Merge cleaned column into main TSV
-DS2_Human$V1 <- DS2_Human_RPKM_cleaned
-DS2_Macaque$V1 <- DS2_Macaque_cleaned
+#Change ENSEMBL ID to Gene ID
+#Note: needs for loop
+DS2_Human$gene_ID <- mapIds(org.Hs.eg.db, keys = DS2_Human$V1, keytype = "ENSEMBL", column="SYMBOL")
+DS2_Macaque$geneID <- mapIds(org.Hs.eg.db, keys = DS2_Macaque$V1, keytype = "ENSEMBL", column="SYMBOL")
+DS1_Human$gene_ID <- mapIds(org.Hs.eg.db, keys = DS1_Human$V1, keytype = "ENSEMBL", column="SYMBOL")
+DS1_Macaque$gene_ID <- mapIds(org.Hs.eg.db, keys = DS1_Macaque$V1, keytype = "ENSEMBL", column="SYMBOL")
+DS1_Chimp$gene_ID <- mapIds(org.Hs.eg.db, keys = DS1_Chimp$V1, keytype = "ENSEMBL", column="SYMBOL")
 
-#Change ENSEMBL ID to Gene ID for human
-DS2_human_geneID <- mapIds(org.Hs.eg.db, keys = DS2_Human$V1, keytype = "ENSEMBL", column="SYMBOL")
-DS2_macaque_geneID <- mapIds(org.Hs.eg.db, keys = DS2_Macaque$V1, keytype = "ENSEMBL", column="SYMBOL")
-DS1_Human$gene_ID <- mapIds(org.Hs.eg.db, keys = DS1_Human$ENSEMBL_ID, keytype = "ENSEMBL", column="SYMBOL")
-DS1_Macaque$gene_ID <- mapIds(org.Hs.eg.db, keys = DS1_Macaque$ENSEMBL_ID, keytype = "ENSEMBL", column="SYMBOL")
-
-#Add ENSEMBL ID to table
-DS2_Human$Gene_ID <- DS2_human_geneID
-DS2_Macaque$Gene_ID <- DS2_macaque_geneID
+#Reorder
+DS1_Human <- DS1_Human[, c(1, 19, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13,14,15,16,17,18)]
+DS1_Chimp <- DS1_Chimp[, c(1, 19, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13,14,15,16,17,18)]
+DS1_Macaque <- DS1_Macaque[, c(1, 19, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,13,14,15,16,17,18)]
+DS2_Human <- DS2_Human[, c(1, 12, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)]
+DS2_Macaque <- DS2_Macaque[, c(1, 12, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)]
---
+++ R Scripts/Preprocessing Zeng et al.R
@@ -0,0 +1,5 @@
+#Preprocessing Zeng et al 
+
+#Load required libraries
+
+library(tidyverse)
