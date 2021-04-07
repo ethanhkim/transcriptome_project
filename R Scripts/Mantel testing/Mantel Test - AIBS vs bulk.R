5f74eccb19ba70ea9be6e21dd3d75a482683902c
Ethan <ethan.kim@mail.utoronto.ca>
2021-04-07 12:41:03 -0400

    SCC - update mantel test script

--- R Scripts/Mantel testing/Mantel Test - AIBS vs bulk.R
+++ R Scripts/Mantel testing/Mantel Test - AIBS vs bulk.R
@@ -30,6 +30,8 @@ load(here("Data", "processed_data", "Allen_logCPM_filtered_dataset.Rdata"))
 load(here("Data", "processed_data", "Zeng_dataset_long.Rdata"))
 Zeng_marker_genes <- unique(Zeng_dataset_long$gene_symbol)
 
+mantel_test(He_DS1_logCPM_dataset, Maynard_logCPM_dataset)
+
 # Remove WM from He and Maynard
 He_DS1_logCPM_dataset %<>% select(-WM)
 He_DS1_logCPM_filtered_dataset %<>% select(-WM)
@@ -68,103 +70,91 @@ for (type in c("GABAergic", "Glutamatergic", "Non-neuronal")) {
 }
 
 
+
 # Mantel tests b/w He, Maynard, Allen ----
 
 ## He et al vs. Allen ## ---- 
 
-# He logCPM vs Allen logCPM - GABAergic
-# Mantel r: -0.1077, p < 0.001, n = 30,744
+## He logCPM vs Allen logCPM ----
+# GABAergic -     Mantel r: -0.1194, p < 0.001, n = 30,744
+# Glutamatergic - Mantel r: -0.1526, p < 0.001, n = 30,744
+# Non-neuronal -  Mantel r: 0.1025, p < 0.001, n = 30,744
 He_AIBS_logCPM <- list()
 for (type in c("GABAergic", "Glutamatergic", "Non-neuronal")) {
   He_AIBS_logCPM[[type]] <- mantel_test(He_DS1_logCPM_dataset,
                                         Allen_cell_type_df[[type]])
 }
+for (type in c("GABAergic", "Glutamatergic", "Non-neuronal")) {
+  print(He_AIBS_logCPM[[type]]$statistic)
+}
 
-# He logCPM vs Allen logCPM - Glutamatergic
-# Mantel r: -0.1295, p < 0.001, n = 30,744
-He_AIBS_GLUT <- mantel_test(He_DS1_logCPM_dataset, Allen_cell_type_df$GABAergic)
+## He logCPM vs Allen logCPM - filtered for CPM > 0.1  ---- 
+# GABAergic -     Mantel r: -0.005578, p < 0.001, n = 13,114
+# Glutamatergic - Mantel r: 0.003160, p < 0.001, n = 13,714
+# Non-neuronal -  Mantel r: 0.06069, p < 0.001, n = 11,645
+He_AIBS_logCPM_filtered <- list()
+for (type in c("GABAergic", "Glutamatergic", "Non-neuronal")) {
+  He_AIBS_logCPM_filtered[[type]] <- mantel_test(He_DS1_logCPM_filtered_dataset,
+                                        Allen_cell_type_filtered_df[[type]])
+}
+for (type in c("GABAergic", "Glutamatergic", "Non-neuronal")) {
+  print(He_AIBS_logCPM_filtered[[type]]$statistic)
+}
 
-# He logCPM vs Allen logCPM - Non-neuronal
-# Mantel r: 0.09004, p < 0.001, n = 30,744
-He_AIBS_NONN <- mantel_test(He_DS1_logCPM_dataset, Allen_NONN)
-
-## He et al vs. Allen - filtered for CPM > 0.1 ## ----
-
-# He  vs Allen - GABAergic
-# Mantel r: -0.006967, p < 0.001, n = 13,114
-mantel_test(He_DS1_logCPM_filtered_dataset, 
-            Allen_GABA_filtered)
-
-# He  vs Allen - Glutamatergic
-# Mantel r: -0.005718, p < 0.001, n = 13,114
-mantel_test(He_DS1_logCPM_filtered_dataset, 
-            Allen_GLUT_filtered)
-
-# He  vs Allen - Non-neuronal
-# Mantel r: 0.05965, p < 0.001, n = 11,645
-mantel_test(He_DS1_logCPM_filtered_dataset, 
-            Allen_NONN_filtered)
-
-## He et al vs. Allen - filtered for Zeng ----
-
-# He vs. Allen - GABAergic
-# Mantel r: 0.03838, p < 0.001, n = 975
-mantel_test(He_Zeng, Allen_GABA_Zeng)
-
-# He vs. Allen - Glutamatergic
-# Mantel r: 0.03376, p < 0.001, n = 975
-mantel_test(He_Zeng, Allen_GLUT_Zeng)
-
-# He vs. Allen - Non-neuronal
-# Mantel r: 0.07922, p < 0.001, n = 975
-mantel_test(He_Zeng, Allen_NONN_Zeng)
+## He logCPM vs Allen logCPM - Zeng subset ----
+# GABAergic -     Mantel r: 0.02548, p < 0.001, n = 13,114
+# Glutamatergic - Mantel r: 0.03520, p < 0.001, n = 13,114
+# Non-neuronal -  Mantel r: 0.07255, p < 0.001, n = 11,645
+He_AIBS_Zeng <- list()
+for (type in c("GABAergic", "Glutamatergic", "Non-neuronal")) {
+  He_AIBS_Zeng[[type]] <- mantel_test(He_Zeng, Allen_cell_type_Zeng[[type]])
+}
+for (type in c("GABAergic", "Glutamatergic", "Non-neuronal")) {
+  print(He_AIBS_Zeng[[type]]$statistic)
+}
 
 
 ## Maynard et al vs. Allen ## ----
 
-# Maynard logCPM vs Allen logCPM - GABAergic
-# Mantel r: -0.01428, p < 0.001, n = 17,197
-mantel_test(Maynard_logCPM_dataset, Allen_GABA)
-
-# Maynard logCPM vs Allen logCPM - Glutamatergic
-# Mantel r: -0.0143, p < 0.001, n = 17,197
-mantel_test(Maynard_logCPM_dataset, Allen_GLUT)
-
-# Maynard logCPM vs Allen logCPM - Non-neuronal
-# Mantel r: 0.02628, p < 0.001, n = 17,197
-mantel_test(Maynard_logCPM_dataset, Allen_NONN)
+## Maynard logCPM vs Allen logCPM ----
+# GABAergic -     Mantel r: -0.01683, p < 0.001, n = 17,197
+# Glutamatergic - Mantel r: -0.01113, p < 0.001, n = 17,197
+# Non-neuronal -  Mantel r: 0.02406, p < 0.001, n = 17,197
+Maynard_AIBS_logCPM <- list()
+for (type in c("GABAergic", "Glutamatergic", "Non-neuronal")) {
+  Maynard_AIBS_logCPM[[type]] <- mantel_test(Maynard_logCPM_dataset,
+                                        Allen_cell_type_df[[type]])
+}
+for (type in c("GABAergic", "Glutamatergic", "Non-neuronal")) {
+  print(Maynard_AIBS_logCPM[[type]]$statistic)
+}
 
 
 ## Maynard et al vs. Allen - filtered for CPM > 0.1 ## ----
+# GABAergic -     Mantel r: 0.006462, p < 0.001, n = 12,913
+# Glutamatergic - Mantel r: 0.002535, p < 0.001, n = 13,614
+# Non-neuronal -  Mantel r: 0.03572, p < 0.001, n = 11,471
+Maynard_AIBS_logCPM_filtered <- list()
+for (type in c("GABAergic", "Glutamatergic", "Non-neuronal")) {
+  Maynard_AIBS_logCPM_filtered[[type]] <- mantel_test(Maynard_logCPM_filtered_dataset,
+                                             Allen_cell_type_filtered_df[[type]])
+}
+for (type in c("GABAergic", "Glutamatergic", "Non-neuronal")) {
+  print(Maynard_AIBS_logCPM_filtered[[type]]$statistic)
+}
 
-# Maynard  vs Allen - GABAergic
-# Mantel r: 0.002822, p < 0.001, n = 12,913
-mantel_test(Maynard_logCPM_filtered_dataset, 
-            Allen_GABA_filtered)
-
-# Maynard  vs Allen - Glutamatergic
-# Mantel r: 0.0097692, p < 0.001, n = 13,614
-mantel_test(Maynard_logCPM_filtered_dataset, 
-            Allen_GLUT_filtered)
-
-# Maynard  vs Allen - Non-neuronal
-# Mantel r: 0.04014, p < 0.001, n = 11,471
-mantel_test(Maynard_logCPM_filtered_dataset, 
-            Allen_NONN_filtered)
 
 ## Maynard et al vs. Allen - filtered for Zeng ## ----
+# GABAergic -     Mantel r: 0.008808, p < 0.001, n = 929
+# Glutamatergic - Mantel r: 0.07633, p < 0.001, n = 929
+# Non-neuronal -  Mantel r: 0.04006, p < 0.001, n = 929
+Maynard_AIBS_Zeng <- list()
+for (type in c("GABAergic", "Glutamatergic", "Non-neuronal")) {
+  Maynard_AIBS_Zeng[[type]] <- mantel_test(Maynard_Zeng, Allen_cell_type_Zeng[[type]])
+}
+for (type in c("GABAergic", "Glutamatergic", "Non-neuronal")) {
+  print(Maynard_AIBS_Zeng[[type]]$statistic)
+}
 
-# Maynard vs. Allen - GABAergic
-# Mantel r: 0.03121, p < 0.001, n = 929
-mantel_test(Maynard_Zeng, Allen_GABA_Zeng)
 
-# Maynard vs. Allen - Glutamatergic
-# Mantel r: 0.064, p < 0.001, n = 929
-mantel_test(Maynard_Zeng, Allen_GLUT_Zeng)
-
-# Maynard vs. Allen - Non-neuronal
-# Mantel r: 0.06274, p < 0.001, n = 929
-Maynard_AIBS_Zeng_NONN <- mantel_test(Maynard_Zeng, Allen_NONN_Zeng)
-
-mantel_test(Maynard_logCPM_dataset, He_DS1_logCPM_dataset)
 
